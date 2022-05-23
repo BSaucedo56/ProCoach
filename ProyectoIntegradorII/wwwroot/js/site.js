ab1519f4e69025755cd3ee6db43462263ccb60f3
Bryan <bsaucedo250300@gmail.com>
2022-05-22 23:52:40 -0500

    Version 2.1

--- ProyectoIntegradorII/wwwroot/js/site.js
+++ ProyectoIntegradorII/wwwroot/js/site.js
@@ -5,3 +5,38 @@
 
 $(".select-add-placeholder").prepend("<option value='' disabled selected>Seleccione una opción</option>");
 
+jQuery(function ($) {
+
+    $(".sidebar-dropdown > a").click(function () {
+        $(".sidebar-submenu").slideUp(200);
+        if (
+            $(this)
+                .parent()
+                .hasClass("active")
+        ) {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this)
+                .parent()
+                .removeClass("active");
+        } else {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this)
+                .next(".sidebar-submenu")
+                .slideDown(200);
+            $(this)
+                .parent()
+                .addClass("active");
+        }
+    });
+
+    $("#close-sidebar").click(function () {
+        $(".page-wrapper").removeClass("toggled");
+    });
+    $("#show-sidebar").click(function () {
+        $(".page-wrapper").addClass("toggled");
+    });
+
+
+
+
+});
